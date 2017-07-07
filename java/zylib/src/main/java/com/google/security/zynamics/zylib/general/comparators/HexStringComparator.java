33a2deda370eca2b3c9523d63900bcc422eecc81
Christian Blichmann <cblichmann@google.com>
2017-07-07 13:36:12 +0200

    Internal change
    PiperOrigin-RevId: 161189046
    Change-Id: I1a78da6cfae9fb2526d71f56be93f0a33d594ed3

--- java/zylib/src/main/java/com/google/security/zynamics/zylib/general/comparators/HexStringComparator.java
+++ java/zylib/src/main/java/com/google/security/zynamics/zylib/general/comparators/HexStringComparator.java
@@ -1,25 +1,13 @@
-// Copyright 2011 Google Inc. All Rights Reserved.
-
 package com.google.security.zynamics.zylib.general.comparators;
 
 import com.google.security.zynamics.zylib.disassembly.CAddress;
-
-import java.io.Serializable;
 import java.util.Comparator;
 
-
-public class HexStringComparator implements Comparator<String>, Serializable {
-  /**
-   * Used for serialization
-   */
-  private static final long serialVersionUID = 8204731939693038511L;
+/** Compares two hexadecimal strings, typically memory addresses. */
+public class HexStringComparator implements Comparator<String> {
 
   @Override
   public int compare(final String o1, final String o2) {
-    if (o1.isEmpty() || o2.isEmpty()) {
-      return o1.equals(o2) ? 0 : o2.isEmpty() ? 1 : -1;
-    }
-
-    return CAddress.compare(Long.valueOf(o1, 16), Long.valueOf(o2, 16));
+    return new CAddress(o1, 16).compareTo(new CAddress(o2, 16));
   }
 }
