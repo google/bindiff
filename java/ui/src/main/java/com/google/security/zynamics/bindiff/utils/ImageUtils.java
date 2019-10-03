854f9cffc23ad5071a7bbd22d2005b7d0fc2e730
Christian Blichmann <cblichmann@google.com>
2019-10-03 13:13:47 +0200

    Internal change
    PiperOrigin-RevId: 272632063
    Change-Id: Icee3a1c2bab7fe05dc7218e1107ce46d39753644

--- java/ui/src/main/java/com/google/security/zynamics/bindiff/utils/ImageUtils.java
+++ java/ui/src/main/java/com/google/security/zynamics/bindiff/utils/ImageUtils.java
@@ -5,11 +5,25 @@ import com.google.security.zynamics.bindiff.log.Logger;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
+import java.util.Arrays;
 import javax.swing.ImageIcon;
 
 public class ImageUtils {
   public static Image getImage(final String imagePath) {
-    final URL imageUrl = Launcher.class.getResource(imagePath);
+    final String pkgName = Launcher.class.getPackage().getName().replace('.', '/');
+    URL imageUrl = null;
+    // Blaze packages up resource files differently than Maven. Search "ui" and "zylib" as well.
+    for (String path :
+        Arrays.asList(
+            "",
+            "/ui/src/main/resources/" + pkgName + "/",
+            "/zylib/src/main/resources/" + pkgName + "/")) {
+      imageUrl = Launcher.class.getResource(path + imagePath);
+      if (imageUrl != null) {
+        break;
+      }
+    }
+
     if (imageUrl == null) {
       Logger.logWarning("Image resource not found: \"" + imagePath + "\"");
       return null;
