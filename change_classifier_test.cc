831f16ff29d52dc95fe38a6a36d728cb2927deac
Christian Blichmann <cblichmann@google.com>
2020-01-07 09:17:34 +0100

    Internal change
    PiperOrigin-RevId: 288446081
    Change-Id: I3ddae3e373460b99fb54b1eafcf508a9b406b67c

---
+++ change_classifier_test.cc
@@ -0,0 +1,30 @@
+#include "third_party/zynamics/bindiff/change_classifier.h"
+
+#ifndef GOOGLE
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#else
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#endif
+
+using ::testing::StrEq;
+
+namespace security::bindiff {
+namespace {
+
+TEST(ChangeClassifierTest, ChangeDescription) {
+  EXPECT_THAT(GetChangeDescription(CHANGE_NONE), StrEq("-------"));
+  EXPECT_THAT(GetChangeDescription(
+                  static_cast<ChangeType>(CHANGE_STRUCTURAL | CHANGE_OPERANDS |
+                                          CHANGE_ENTRYPOINT | CHANGE_CALLS)),
+              StrEq("G-O-E-C"));
+  EXPECT_THAT(GetChangeDescription(static_cast<ChangeType>(
+                  CHANGE_STRUCTURAL | CHANGE_INSTRUCTIONS | CHANGE_OPERANDS |
+                  CHANGE_BRANCHINVERSION | CHANGE_ENTRYPOINT | CHANGE_LOOPS |
+                  CHANGE_CALLS)),
+              StrEq("GIOJELC"));
+}
+
+}  // namespace
+}  // namespace security::bindiff
